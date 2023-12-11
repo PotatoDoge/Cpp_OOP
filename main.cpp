4b962d391cf6e1a2476dc818dbc2c1917a0f9c29
bprado <bastian.prado.v@gmail.com>
2023-12-10 22:48:55 -0800

    OOP basics

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 
-class Employee {
+
+class AbstractEmployee{
+    virtual void askForPromotion() = 0;
+};
+
+class Employee: AbstractEmployee {
 private:
     string name;
     string lastName;
@@ -41,7 +46,7 @@ public:
         this->age = age;
     }
 
-    const string getCompany() {
+    string getCompany() {
         return this->company;
     }
 
@@ -55,14 +60,81 @@ public:
         cout << "Age: " << age << endl;
         cout << "Company: " << company << endl;
     }
+
+    void askForPromotion() override{
+        if(this->age > 30){
+            cout << this->name << " got promoted!" << endl;
+        }
+        else{
+            cout << this->name << " did not get promoted" << endl;
+        }
+    }
+
+     virtual void work(){
+        cout << "Working" << endl;
+    }
+
+};
+
+class Developer: public Employee{
+private:
+    string favLanguage;
+public:
+    Developer(string name, string lastName, int age, string company, string favLanguage): Employee(name, lastName, age, company){
+        this->favLanguage = favLanguage;
+    }
+
+    void fixBug(){
+        cout << "Bug fixed by " << this->getName() + " " + this->getLastName() << endl;
+    }
+
+    void work(){
+        cout << "Working in a new software" << endl;
+    }
+
+};
+
+class Teacher: public Employee{
+private:
+    string subject;
+public:
+    Teacher(string name, string lastName, int age, string company, string subject): Employee(name, lastName, age, company){
+        this->subject = subject;
+    }
+
+    void prepareLesson(){
+        cout << getName() + " " + getLastName() << " is preparing " << this->subject << " lesson." << endl;
+    }
+
+    void work(){
+        cout << "Teaching" << endl;
+    }
 };
 
 
 int main() {
-    Employee jane = Employee("Jane","Doe",32,"Meta");
-    jane.setName("juan");
+    Employee jane = Employee("Jane", "Doe", 32, "Meta");
+    jane.setName("Jenny");
     jane.introduceYourself();
+    jane.askForPromotion();
 
-    Employee john = Employee("John","Doe",25,"Google");
+    Employee john = Employee("John", "Doe", 25, "Google");
     john.introduceYourself();
+    john.askForPromotion();
+
+    Developer developer = Developer("Paul", "Stones", 65, "X", "Java");
+    developer.fixBug();
+    developer.askForPromotion();
+    developer.work();
+
+    Teacher teacher = Teacher("Rose","McDonald",45,"UNAM","History");
+    teacher.prepareLesson();
+    teacher.work();
+
+    Employee *d = &developer;
+    Employee *t = &teacher;
+    d->work();
+    t->work();
+
+
 }
